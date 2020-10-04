6647bf373ee5639f52dd1cb30493b1cae3f7c6ba
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-10-04 21:19:12 +0800

    登录注册页面修改2 加入mbox

--- pc/render/js/pages/LoginOrRegister/index.js
+++ pc/render/js/pages/LoginOrRegister/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import classes from './style.css';
+import {inject, observer} from 'mobx-react';
 
+@inject(stores => ({
+    doLogin: stores.session.loginAction
+}))
+@observer
 export default class LoginOrRegister extends Component {
     constructor(props) {
         super(props);
@@ -23,9 +28,9 @@ export default class LoginOrRegister extends Component {
         return (
             <div>
                 <div>账号：</div>
-                <input />
+                <input name="username" type="text" onChange={this.onInputChange} />
                 <div>密码：</div>
-                <input />
+                <input name="password" onChange={e => this.onInputChange(e)} />
                 <button onClick={() => this.onLoginClick()}>登录</button>
                 <button onClick={() => this.toggle()}>去注册</button>
             </div>
@@ -36,6 +41,13 @@ export default class LoginOrRegister extends Component {
         return (
             <div>
                 <div>Register page</div>
+                <div>账号：</div>
+                <input />
+                <div>密码：</div>
+                <input />
+                <div>确认密码：</div>
+                <input />
+                <button onClick={() => this.onRegisterClick()}>注册</button>
                 <button onClick={() => this.toggle()}>去登录</button>
             </div>
         );
@@ -48,6 +60,17 @@ export default class LoginOrRegister extends Component {
     }
 
     onLoginClick() {
+        this.props.doLogin();
         console.log('onLoginClick.');
+    }
+
+    onRegisterClick() {
+        console.log('onRegisterClick.');
+    }
+
+    onInputChange(e) {
+        let inputValue = e.target.value;
+        let inputName = e.target.name;
+        console.log('-->  ' + inputName + ' : ' + inputValue);
     }
 }
